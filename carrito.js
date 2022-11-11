ae177ae882e8451ccd8aa06eee07daecae7e6a60
Maico <maico@MacBook-Pro-de-Maico.local>
2022-11-10 23:03:33 -0300

    minor fix

--- carrito.js
+++ carrito.js
@@ -1,158 +1,116 @@
-const fs = require("fs")
+const fs = require("fs");
+
 module.exports = class Contenedor {
-    constructor(file) {
-        this.file = file;
+  constructor(file) {
+    this.file = file;
+  }
+  async getAll() {
+    try {
+      const check = await fs.promises.readFile(`${this.file}`, "utf-8");
+      return JSON.parse(check);
+    } catch (error) {
+      console.log("El archivo está vacío");
+      await fs.promises.writeFile(this.file, "");
+      const check = await fs.promises.readFile(this.file, "utf-8");
+      return check;
     }
-    getAll = async () => {
+  }
+  async saveAll(productoingresado) {
+    let arrayprevio = await this.getAll();
+    let idPrevio = () => {
+      if (arrayprevio.length > 0) {
+        return arrayprevio.length;
+      } else {
+        arrayprevio = [];
+        return 0;
+      }
+    };
+    productoingresado.forEach((value, index) => {
+      value.id = index + idPrevio() + 1;
+    });
+    arrayprevio.push(...productoingresado);
 
-        try {
-            const check = await fs.promises.readFile(`${this.file}`, 'utf-8');
-            return JSON.parse(check);
-
-        } catch (error) {
-            console.log('El archivo está vacío');
-            await fs.promises.writeFile(this.file, "");
-            const check = await fs.promises.readFile(this.file, 'utf-8');
-            return check;
-
-        }
-
+    try {
+      await fs.promises.writeFile(
+        `${this.file}`,
+        JSON.stringify(arrayprevio, null, 2)
+      );
+      return console.log(`Nuevo item agregado con exito!`);
+    } catch (error) {
+      throw new Error("Error en el guardado");
     }
-    saveAll = async (productoingresado) => {
-        let arrayprevio = await this.getAll();
-        let idPrevio = () => {
-            if (arrayprevio.length > 0) {
-                const ids = arrayprevio.map(object => {
-                    return object.id;
-                })
-                const max = Math.max(...ids);
-                return max;
-            } else {
-                arrayprevio = [];
-                return 0;
+  }
 
-            }
+  async getById(nro) {
+    try {
+      const check = await this.getAll();
+      let validate = 0;
+      check.forEach((element) => {
+        if (element.id == nro) {
+          validate++;
+          console.log("Exito, producto encontrado");
+          return element;
         }
-        productoingresado.forEach((value, index) => {
-            value.id = index + idPrevio() + 1;
-            value.price = parseInt(value.price);
-            value.stock = parseInt(value.stock);
-        })
-        arrayprevio.push(...productoingresado);
-
-        try {
-            await fs.promises.writeFile(`${this.file}`, JSON.stringify(arrayprevio, null, 2));
-            return console.log(`Nuevo item agregado con exito!`);
-        } catch (error) {
-            throw new Error('Error en el guardado');
-        }
+      });
+      if (validate == 0) {
+        return "No existe el producto";
+      }
+    } catch {
+      throw new Error("Error al obtener el Id");
     }
+  }
 
-    getById = async (nro) => {
-        try {
-            const check = await this.getAll();
-            let validate = 0;
-            let producto = {};
-            check.forEach((element) => {
-                if (element.id == nro) {
-                    validate++;
-                    producto = element;
-                    console.log('Exito, producto encontrado')
-                }
-            })
-            if (validate == 0) {
-                throw new Error('No existe el producto')
-            }
-            console.log(producto)
-            return producto
-        } catch (err) {
-            throw new Error('Error al obtener el Id :' + err);
-        }
+  async getRandom() {
+    try {
+      const check = await this.getAll();
+      let random = Math.floor(Math.random() * check.length);
+      return check[random];
+    } catch {
+      throw new Error("Error al obtener productos aleatorios");
     }
+  }
 
-    getRandom = async () => {
-        try {
-            const check = await this.getAll();
-            let random = Math.floor(Math.random() * check.length);
-            console.log('Exito, producto encontrado')
-            return check[random];
-        } catch (err) {
-            throw new Error('Error al obtener productos aleatorios: ' + err);
+  async deleteById(nro) {
+    try {
+      const check = await this.getAll();
+      check.forEach((element) => {
+        if (nro == element.id) {
+          check.splice(check.indexOf(element), 1);
         }
+      });
+      await fs.promises.writeFile(this.file, JSON.stringify(check, null, 2));
+    } catch {
+      throw new Error("Error en el borrado de Id");
     }
+  }
 
-    deleteById = async (nro) => {
-        try {
-            const check = await this.getAll();
-            if (check.some((element) => element.id == nro)) {
-                check.forEach(element => {
-                    if (nro == element.id) {
-                        check.splice(check.indexOf(element), 1);
-                        console.log('Exito, producto eliminado')
-                    }
-                })
-                await fs.promises.writeFile(this.file, JSON.stringify(check, null, 2));
-            } else {
-                throw new Error('No existe el producto')
-            }
-        } catch (err) {
-            throw new Error('Error al borrar: ' + err)
-        }
+  async deleteAll() {
+    try {
+      await fs.promises.writeFile(this.file, ""); //JSON.stringify([], null, 2);
+      console.log("Todos los productos fueron eliminados");
+    } catch {
+      throw new Error("No se pudieron borrar todos los objetos");
     }
+  }
 
-    deleteAll = async () => {
-        try {
-            await fs.promises.writeFile(this.file, "") //JSON.stringify([], null, 2);
-            console.log('Todos los productos fueron eliminados');
-        } catch (err) {
-            throw new Error('No se pudieron borrar todos los objetos' + err);
+  async sellById(nro) {
+    try {
+      let check = await this.getAll();
+      let validate = 0;
+      check.forEach((element) => {
+        if (nro == element.id) {
+          element.stock--;
+          validate++;
+          console.log("El producto se vendio con exito");
+          console.log("Quedan " + element.stock + " unidades");
         }
+      });
+      if (validate == 0) {
+        console.log("No existe el producto o no hay stock");
+      }
+      await fs.promises.writeFile(this.file, JSON.stringify(check, null, 2));
+    } catch {
+      throw new Error("Error en la venta");
     }
-
-    sellById = async (nro) => {
-        try {
-            let check = await this.getAll();
-            if (check.some((element) => element.id == nro & element.stock > 0)) {
-                let validate = 0;
-                check.forEach(element => {
-                    element.stock--;
-                    validate++;
-                    console.log('El producto se vendio con exito');
-                    console.log('Quedan ' + element.stock + ' unidades');
-                })
-                await fs.promises.writeFile(this.file, JSON.stringify(check, null, 2));
-            } else {
-                if (check.some((element) => element.id == nro) == False) {
-                    throw new Error('El producto no existe')
-                }
-                if (check.some((element) => element.id == nro & element.stock < 0)) {
-                    throw new Error('El producto no tiene stock')
-                }
-            }
-        } catch (err) {
-            throw new Error('Error en la venta' + err);
-        }
-
-    }
-    modifyById = async (nro, modified) => {
-        try {
-            const check = await this.getAll();
-            if (check.some((element) => element.id == nro)) {
-                check.forEach(element => {
-                    if (nro == element.id) {
-                        element.title = modified.title;
-                        element.price = parseInt(modified.price);
-                        element.stock = parseInt(modified.stock);
-                        element.thumbnail = modified.thumbnail;
-                        console.log('El producto se modifico con exito');
-                    }
-                })
-                await fs.promises.writeFile(this.file, JSON.stringify(check, null, 2));
-            } else {
-                throw new Error('No existe el producto');
-            }
-        } catch (err) {
-            throw new Error('Error al modificar el producto: ' + err);
-        }
-    }
-}
+  }
+};
--- server.js
+++ server.js
@@ -1,38 +1,52 @@
 const express = require('express')
+const moment = require('moment')
+const Contenedor = require ('./carrito.js')
+
 const app = express()
-const {engine}= require('express-handlebars')
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-app.engine('hbs', engine({
-    extname: 'hbs',
-    defaultLayout: 'default',
-    layoutDir: "/views/layouts",
-  }))
- 
-app.set('view engine', 'hbs');
-app.set('views', "./views");
-
-const productos = require('./productos.js')
-
-app.use(express.static('public'));
-app.use ('/api/productos', productos)
-
-app.get("/",(req, res) => {
-    try {
-        res.render('./construccion.hbs')
-    } catch (err) {
-        res.status(500).send('Error en el proceso GET :' + err)
-    }
-}) 
-
-const port = 8080
+const port = process.env.PORT || 3000
 const server = app.listen(port,()=>{
         console.log('Servidor corriendo en el puerto: ' + server.address().port)
     })
 
 server.on('error', (error)=>{
     console.log(`Error en el servidor ${error}`)
+})
+
+app.get('/', (req, res)=>{
+    res.send(hola)
+})
+
+const hola = `<h1>Bienvenido al escueto Mercado Virtual de Pulgas de elementos obsoletos informáticos</h1>\n
+<p>Para ver los productos disponibles, haga click <a href=/productos>aquí</a></p>\n
+<p><a href = "/productoRandom">Voy a tener suerte</a></p>\n
+<p><a href = "/fyh">Quiero saber la hora</a></p>\n
+<p><a href = "/visitas">Número de visitas</a></p>\n
+`
+
+let visitas = 0
+
+let stock = new Contenedor('productos.txt');
+
+ app.get('/productos', async (req, res)=> {
+    const productos = await stock.getAll();
+    res.send(productos)
+    }
+)
+
+app.get('/productoRandom', async (req, res)=>{
+    let producto = await stock.getRandom()    
+    res.send(producto)
+    }
+)
+
+app.get('/visitas', (req, res)=>{
+    visitas++
+    res.send(`La cantidad de visitas es ${visitas}`)
+    }
+)
+
+app.get('/fyh', (req, res)=>{
+    const fyh = moment().format('YYYY/MM/DD HH:mm:ss')
+    res.send(`La hora actual es: ${fyh}`)
 })
 
